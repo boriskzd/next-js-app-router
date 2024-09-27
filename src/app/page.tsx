af4e8c38845ade29efde4fff5c6d9f3bc74aadbf
boriskzd <boriskzd@users.noreply.github.com>
2024-09-27 21:39:12 +0200

    add client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+// Next
 import Image from 'next/image';
+// Components
+import LikeButton from './like-button';
 
 export default function Home() {
+	const myList = ['Archipelago', 'Beach', 'Coast', 'Desolation'];
+
 	return (
 		<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
 			<main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
@@ -13,7 +18,13 @@ export default function Home() {
 					height={38}
 					priority
 				/>
+
+				<LikeButton />
+
 				<ol className='list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]'>
+					{myList.map((item) => (
+						<li className='mb-2'>{item}</li>
+					))}
 					<li className='mb-2'>
 						Get started by editing{' '}
 						<code className='bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold'>
@@ -21,7 +32,6 @@ export default function Home() {
 						</code>
 						.
 					</li>
-					<li>Save and see your changes instantly.</li>
 				</ol>
 
 				<div className='flex gap-4 items-center flex-col sm:flex-row'>
