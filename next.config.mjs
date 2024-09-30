4af2e7879154607aec376c1dcb520344201dc0db
boriskzd <boriskzd@users.noreply.github.com>
2024-09-30 19:34:51 +0200

    add option to remove  console.log and other calls
    when the code is uncommented, console.logs will no longer be displayed for client or server.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 // Configuration file for Next.js
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	compiler: {
+		// remove all console calls, except 'error', 'info', 'warn' since they are important
+		// removeConsole: process.env.NodeDev === "production"
+		// ----- UNCOMMENT CODE BELOW TO REMOVE console.xxx() MESSAGES -----
+		// removeConsole: {
+		// 	exclude: ['error', 'info', 'warn'],
+		// },
+	},
+};
 
 export default nextConfig;
